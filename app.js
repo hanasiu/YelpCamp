3f22c3a9f97da36aa6078449fec2fa777ad2f36b
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-10-14 17:25:03 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
 
-const catchAsync = require('./utils/catchAsync');
+
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+//const catchAsync = require('./utils/catchAsync');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const { Passport } = require('passport');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -48,14 +53,28 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    console.log(req.session);
+    // if(!['/login', '/'].includes(req.originalUrl)) {
+    //     req.session.returnTo = req.originalUrl;
+    // }
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
